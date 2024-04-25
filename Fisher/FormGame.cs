a17ff52febcac89e91c44fc46a815e18f0da512e
user <mishakopytok@gmail.com>
2024-04-26 01:02:22 +0300

    init comit

---
+++ Fisher/FormGame.cs
@@ -0,0 +1,47 @@
+﻿using System.ComponentModel;
+
+namespace Fisher;
+
+public partial class FormGame : Form
+{
+    private Game _game;
+    private Button btnReady;
+    private Label lInfo;
+    public FormGame(Game game)
+    {
+        InitializeComponent();
+        Size = new Size(1800, 1000);
+        btnReady = new Button();
+        btnReady.Top = 600;
+        btnReady.Left = 490;
+        btnReady.Size = new Size(100, 40);
+        btnReady.Text = "Готов";
+        btnReady.Click += btnReady_Click;
+        Controls.Add(btnReady);
+        btnReady.Hide();
+
+        lInfo = new Label();
+        lInfo.Text = "Ожидание оппонента...";
+        lInfo.Left = 300;
+        lInfo.Top = 100;
+        lInfo.Size = new Size(400, 30);
+        Controls.Add(lInfo);
+        
+        _game = game;
+        Closing += (sender, args) => { Application.Exit(); };
+        _game.ActionEnemyJoin += EnemyJoin;
+    }
+
+    private void EnemyJoin(string name)
+    {
+        lInfo.Text = "Нажмите готов для начала игры";
+        btnReady.Show();
+    }
+
+    private async void btnReady_Click(object? sender, EventArgs e)
+    {
+        await _game.SendMessage(Game.Ready);
+        btnReady.Hide();
+        lInfo.Text = "Ожидание готовности оппонента...";
+    }
+}
---
+++ Fisher/Game.cs
@@ -0,0 +1,110 @@
+﻿using System.Net.WebSockets;
+using System.Text;
+using Thread = System.Threading.Thread;
+
+namespace Fisher;
+
+public class Game
+{
+    public delegate void MessageDelegate(string message);
+    public delegate void EmptyDelegate();
+
+    public event MessageDelegate? ActionMessageCome;
+    public event MessageDelegate? ActionEnemyJoin;
+    public event EmptyDelegate? ActionConnection;
+    public event EmptyDelegate? ActionGameStart;
+    public event MessageDelegate? ActionEndGame;
+    
+    private ClientWebSocket? _client;
+    
+    public const string Connected = "Connected";
+    public const string Start = "Start";
+    public const string Lose = "Lose";
+    public const string Win = "Win";
+    public const string NoWinner = "No winner";
+    public const string EndGame = "End";
+    public const string Enemy = "Enemy";
+    
+    public const string Ready = "Ready";
+    
+    public async void Connect(string ip, string port)
+    {
+        try
+        {
+            _client = new ClientWebSocket();
+            Uri serverUri = new Uri($"ws://{ip}:{port}/ws");
+            await _client.ConnectAsync(serverUri, CancellationToken.None);
+            StartReceiving();
+            ActionConnection();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+        }
+    }
+    public async Task SendMessage(string message)
+    {
+        byte[] buffer = Encoding.UTF8.GetBytes(message);
+        await _client.SendAsync(new ArraySegment<byte>(buffer), WebSocketMessageType.Text, true, CancellationToken.None);
+    }
+
+    private async void MessageReceiver()
+    {
+        while (_client.State == WebSocketState.Open)
+        {
+            ArraySegment<byte> buffer = new ArraySegment<byte>(new byte[1024]);
+            WebSocketReceiveResult result = await _client.ReceiveAsync(buffer, CancellationToken.None);
+
+            string message = Encoding.UTF8.GetString(buffer.Array != null ? buffer.Array : Array.Empty<byte>(), 0, result.Count);
+            ActionMessageCome(message);
+            // ActionMessageCome!=null?ActionMessageCome(message):;
+            Console.WriteLine("Received message: " + message);
+        }
+        
+         await _client.CloseAsync(WebSocketCloseStatus.EndpointUnavailable, "", CancellationToken.None);
+    }
+
+    private void StartReceiving()
+    {
+        ActionMessageCome += ActionMessageHandler;
+        Thread thread = new Thread(MessageReceiver);
+        thread.Start();
+    }
+
+    private void StopClient()
+    {
+        _client.CloseAsync(WebSocketCloseStatus.NormalClosure, "Method StopClient", CancellationToken.None);
+    }
+
+    private void ActionMessageHandler(string message)
+    {
+        switch (message)
+        {
+            case Start:
+                Console.WriteLine("Game is starting");
+                break;
+            case EndGame:
+                Console.WriteLine("Game is over");
+                ActionEndGame(NoWinner);
+                StopClient();
+                break;
+            case Win:
+                Console.WriteLine("You win");
+                ActionEndGame(Win);
+                StopClient();
+                break;
+            case Lose:
+                Console.WriteLine("Tou lose");
+                ActionEndGame(Lose);
+                StopClient();
+                break;
+            case Connected:
+                Console.WriteLine("Tou are connected");
+                break;
+            case Enemy:
+                // Console.WriteLine("Tou are connected");
+                ActionEnemyJoin(string.Empty);
+                break;
+        }
+    }
+}
