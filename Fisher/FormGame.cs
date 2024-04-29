0746a57c78a6adeca3dfe993a92a39b62239818a
user <mishakopytok@gmail.com>
2024-04-30 00:09:12 +0300

    fish and timer

--- Fisher/FormGame.cs
+++ Fisher/FormGame.cs
@@ -7,8 +7,12 @@ public partial class FormGame : Form
     private Game _game;
     private Button btnReady;
     private Label lInfo;
+    private Label lTime;
     private Label lEnemy;
     private Panel gamePanel;
+    private Panel fishPanel;
+    private int fishWidth = 100;
+    private int fishHeight = 50;
     public FormGame(Game game)
     {
         InitializeComponent();
@@ -29,6 +33,13 @@ public partial class FormGame : Form
         lInfo.Size = new Size(400, 30);
         Controls.Add(lInfo);
         
+        lTime = new Label();
+        lTime.Text = "Время";
+        lTime.Left = 800;
+        lTime.Top = 20;
+        lTime.Size = new Size(200, 30);
+        Controls.Add(lTime);
+        
         lEnemy = new Label();
         lEnemy.Text = "";
         lEnemy.Left = 20;
@@ -40,6 +51,8 @@ public partial class FormGame : Form
         gamePanel.Top = 70;
         gamePanel.Left = 10;
         gamePanel.Size = new Size(ClientSize.Width-20, ClientSize.Height - 80);
+        int maxX = gamePanel.Width - fishWidth;
+        int maxY = gamePanel.Height - fishHeight;
         // string currentDirectory = Directory.GetCurrentDirectory();
         // string imagePath = Path.Combine(currentDirectory, @"images\lake2.jpg");
         gamePanel.BackgroundImage = Image.FromFile(@"..\..\..\images\lake2.jpg");
@@ -50,10 +63,14 @@ public partial class FormGame : Form
         Closing += (sender, args) => { Application.Exit(); };
         _game.ActionEnemyJoin += EnemyJoin;
         _game.ActionGameStart += StartGame;
+        _game.ActionNewFish += NewFish;
+        _game.ActionTimerTick += ShowTime;
+        _game.ActionEndGame += GameOver;
     }
 
     private void EnemyJoin(Game.GameMessage gameMessage)
     {
+        
         Action lAction = () => lInfo.Text = "Нажмите готов для начала игры";
         if (lInfo.InvokeRequired)
             lInfo.Invoke(lAction); 
@@ -76,15 +93,73 @@ public partial class FormGame : Form
 
     private void StartGame()
     {
+        _game.EnemyScore = 0;
+        _game.SelfScore = 0;
         Action lAction = () => lInfo.Text = "Игра началась";
         if (lInfo.InvokeRequired)
             lInfo.Invoke(lAction); 
         else lAction();
-        _game.GameProcessStart(gamePanel.Width, gamePanel.Height);
+        
+        Action lAction1 = () => lTime.Text = "60";
+        if (lTime.InvokeRequired)
+            lTime.Invoke(lAction1); 
+        else lAction1();
+        
+        _game.GameProcessStart();
     }
 
     private void NewFish(int x, int y)
     {
+        Controls.Remove(fishPanel);
+        fishPanel = new Panel();
+        fishPanel.Size = new Size(fishWidth, fishHeight);
+        fishPanel.Left = x;
+        fishPanel.Top = y;
+        fishPanel.BackgroundImage = Image.FromFile(@"..\..\..\images\fish1.png");
+        fishPanel.BackgroundImageLayout = ImageLayout.Stretch;
+        fishPanel.Click += FishClick;
+        
+        Action lAction = () => gamePanel.Controls.Add(fishPanel);
+        if (gamePanel.InvokeRequired)
+            gamePanel.Invoke(lAction); 
+        else lAction();
+        
+        Invalidate();
+    }
+
+    private void FishClick(object? sender, EventArgs e)
+    {
+        _game.SelfScore++;
+        _game.SendMessageFishClick();
+    }
+
+    private void ShowTime(int time)
+    {
+        Action lAction1 = () => lTime.Text = time.ToString();
+        if (lTime.InvokeRequired)
+            lTime.Invoke(lAction1); 
+        else lAction1();
+        Invalidate();
+    }
+
+    private void GameOver(Game.EndGameMessage message)
+    {
+        if (!message.isCorrect)
+        {
+            MessageBox.Show("Возникла ошибка в соединении, ничья");
+        }
+        else if (message.isNoWinner)
+        {
+            MessageBox.Show($"Ничья, оба игрока набрали по {message.winnerScore} очков");
+        }
+        else if (message.isYouWin)
+        {
+            MessageBox.Show($"Вы выиграли, набрав {message.winnerScore} очков, ваш противник набрал {message.loserScore} очков");
+        }
+        else
+        {
+            MessageBox.Show($"Вы проиграли, ваш противник {message.winnerName} набрал {message.winnerScore} очков, вы набрали {message.loserScore} очков");
+        }
         
     }
 }
--- Fisher/Game.cs
+++ Fisher/Game.cs
@@ -3,41 +3,54 @@ using System.Text;
 using Thread = System.Threading.Thread;
 using System.Text.Json;
 using System.Text.Json.Serialization;
+using Timer = System.Threading.Timer;
 
 namespace Fisher;
 
 public class Game
 {
     public delegate void MessageDelegate(GameMessage message);
+    public delegate void EndGameDelegate(EndGameMessage message);
     public delegate void NewFishDelegate(int x, int y);
+    public delegate void TimerDelegate(int time);
     public delegate void EmptyDelegate();
 
     public event MessageDelegate? ActionMessageCome;
     public event MessageDelegate? ActionEnemyJoin;
     public event EmptyDelegate? ActionConnection;
     public event EmptyDelegate? ActionGameStart;
-    public event MessageDelegate? ActionEndGame;
+    public event EndGameDelegate? ActionEndGame;
     public event NewFishDelegate? ActionNewFish;
+    public event TimerDelegate? ActionTimerTick;
     
     private ClientWebSocket? _client;
     
-    public const string Connected = "Connected";
-    public const string Start = "Start";
-    public const string Lose = "Lose";
-    public const string Win = "Win";
-    public const string NoWinner = "No winner";
-    public const string EndGame = "End";
-    public const string Enemy = "Enemy";
+    public const string messageConnected = "Connected";
+    public const string messageStart = "Start";
+    public const string messageEndGame = "End";
+    public const string messageEnemy = "Enemy";
     public const string messageEmpty = "Empty";
+    public const string messageUpdate = "Update";
+    public const string messageNewFish = "Fish";
     private Thread _thread;
+    private Thread receivingThread;
     
     public const string Ready = "Ready";
+    public int EnemyScore;
+    public int SelfScore;
+
+    public volatile int remainingTime;
+    public Timer timer;
+    public string SelfName;
+    public string EnemyName;
 
     public class GameMessage
     {
         public string Command { get; set; }
         public string Name { get; set; }
         public int Scores { get; set; }
+        public int FishX { get; set; }
+        public int FishY { get; set; }
         public GameMessage(){}
         public GameMessage(string command)
         {
@@ -49,6 +62,16 @@ public class Game
             Name = name;
             Scores = scores;
         }
+    }
+
+    public class EndGameMessage
+    {
+        public bool isYouWin;
+        public bool isCorrect;
+        public string winnerName;
+        public bool isNoWinner;
+        public int winnerScore;
+        public int loserScore;
     }
     public async void Connect(string ip, string port)
     {
@@ -73,6 +96,12 @@ public class Game
         await _client.SendAsync(new ArraySegment<byte>(buffer), WebSocketMessageType.Text, true, CancellationToken.None);
     }
 
+    public void SendMessageFishClick()
+    {
+        GameMessage message = new GameMessage("Fish");
+        SendMessage(message);
+    }
+
     private async void MessageReceiver()
     {
         while (_client.State == WebSocketState.Open)
@@ -82,7 +111,7 @@ public class Game
 
             string message = Encoding.UTF8.GetString(buffer.Array != null ? buffer.Array : Array.Empty<byte>(), 0, result.Count);
             GameMessage gameMessage = JsonSerializer.Deserialize<GameMessage>(message);
-            ActionMessageCome(gameMessage==null?new GameMessage(messageEmpty):gameMessage);
+            ActionMessageCome(gameMessage);
             // ActionMessageCome!=null?ActionMessageCome(message):;
             Console.WriteLine("Received message: " + message);
         }
@@ -93,8 +122,8 @@ public class Game
     private void StartReceiving()
     {
         ActionMessageCome += ActionMessageHandler;
-        Thread thread = new Thread(MessageReceiver);
-        thread.Start();
+        receivingThread = new Thread(MessageReceiver);
+        receivingThread.Start();
     }
 
     private void StopClient()
@@ -106,43 +135,95 @@ public class Game
     {
         switch (message.Command)
         {
-            case Start:
+            case messageStart:
                 Console.WriteLine("Game is starting");
                 ActionGameStart();
                 break;
-            case EndGame:
+            case messageEndGame:
                 Console.WriteLine("Game is over");
-                ActionEndGame(message);
+                CreateGameResult(false);
                 StopClient();
                 break;
-            case Win:
-                Console.WriteLine("You win");
-                ActionEndGame(message);
-                StopClient();
-                break;
-            case Lose:
-                Console.WriteLine("Tou lose");
-                ActionEndGame(message);
-                StopClient();
-                break;
-            case Connected:
+            case messageConnected:
                 Console.WriteLine("Tou are connected");
                 break;
-            case Enemy:
-                // Console.WriteLine("Tou are connected");
+            case messageEnemy:
                 ActionEnemyJoin(message);
+                break;
+            case messageUpdate:
+                EnemyScore = message.Scores;
+                break;
+            case messageNewFish:
+                ActionNewFish(message.FishX, message.FishY);
                 break;
         }
     }
 
-    public void GameProcessStart(int width, int height)
+    public void GameProcessStart()
     {
-        _thread = new Thread(() => GameProcessThread(width, height));
+        _thread = new Thread(() => GameProcessThread());
         _thread.Start();
     }
 
-    public void GameProcessThread(int width, int height)
+    private void CreateGameResult(bool isCorrect = true)
     {
-        
+        if (!isCorrect)
+        {
+            EndGameMessage message = new EndGameMessage();
+            message.isCorrect = false;
+            ActionEndGame(message);
+        }
+        if (SelfScore > EnemyScore)
+        {
+            EndGameMessage message = new EndGameMessage();
+            message.isYouWin = true;
+            message.isNoWinner = false;
+            message.winnerScore = SelfScore;
+            message.loserScore = EnemyScore;
+            message.winnerName = SelfName;
+            message.isCorrect = false;
+            ActionEndGame(message);
+        } else if (SelfScore < EnemyScore)
+        {
+            EndGameMessage message = new EndGameMessage();
+            message.isYouWin = false;
+            message.isNoWinner = false;
+            message.winnerScore = EnemyScore;
+            message.loserScore = SelfScore;
+            message.winnerName = EnemyName;
+            message.isCorrect = false;
+            ActionEndGame(message);
+        }
+        else
+        {
+            EndGameMessage message = new EndGameMessage();
+            message.isYouWin = false;
+            message.isNoWinner = true;
+            message.winnerScore = EnemyScore;
+            message.loserScore = SelfScore;
+            message.winnerName = EnemyName;
+            message.isCorrect = false;
+            ActionEndGame(message);
+        }
+    }
+
+    private void GameProcessThread()
+    {
+        remainingTime = 60; // Время в секундах
+        timer = new Timer(TimerCallback, null, 0, 1000); // Запуск таймера с интервалом 1 секунда (1000 миллисекунд)
+    }
+    private void TimerCallback(object state)
+    {
+        remainingTime--;
+
+        if (remainingTime <= 0)
+        {
+            timer.Dispose();
+            CreateGameResult();
+        }
+        else
+        {
+            ActionTimerTick(remainingTime);
+        }
     }
 }
