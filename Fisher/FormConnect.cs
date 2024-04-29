e93b2851cac842a6fbee8fb035a1dfd7f9c41a6b
user <mishakopytok@gmail.com>
2024-04-30 00:09:58 +0300

    fish and timer

---
+++ Fisher/FormConnect.cs
@@ -0,0 +1,48 @@
+using System.Net.WebSockets;
+
+namespace Fisher;
+
+public partial class FormConnect : Form
+{
+    Button btnConnect = new Button();
+    TextBox tbIP = new TextBox();
+    TextBox tbPort = new TextBox();
+    private Game _game;
+    public FormConnect()
+    {
+        InitializeComponent();
+        
+        btnConnect.Top = 300;
+        btnConnect.Left = 300;
+        btnConnect.Text = "Подключиться";
+        btnConnect.Size = new Size(170, 40);
+        btnConnect.Click += btnConnect_Click;
+        Controls.Add(btnConnect);
+        
+        tbIP.Size = new Size(200, 30);
+        tbIP.Top = 100;
+        tbIP.Left = 300;
+        tbIP.Text = "127.0.0.1";
+        Controls.Add(tbIP);
+        
+        tbPort.Size = new Size(200, 30);
+        tbPort.Top = 200;
+        tbPort.Left = 300;
+        tbPort.Text = "8080";
+        Controls.Add(tbPort);
+    }
+
+    private void btnConnect_Click(object? sender, EventArgs e)
+    {
+        _game = new Game();
+        _game.ActionConnection += CreateGameForm;
+        _game.Connect(tbIP.Text, tbPort.Text);
+    }
+
+    private void CreateGameForm()
+    {
+        FormGame formGame = new FormGame(_game);
+        formGame.Show();
+        this.Hide();
+    }
+}
